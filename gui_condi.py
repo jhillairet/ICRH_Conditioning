72df0ace872b0640fef51dfe11af649ade3c54b3
Julien Hillairet <julien.hillairet@cea.fr>
2017-09-27 17:04:18 +0200

    Removed matplotlib for pyqtgraph

--- gui_condi.py
+++ gui_condi.py
@@ -1,8 +1,4 @@
 import sys
-import matplotlib
-from matplotlib.figure import Figure
-import ICRH_Conditioning as condi
-import ICRH_FileIO as io
 import os
 
 # Qt5/Qt4 compatibility
@@ -15,7 +11,7 @@ try:
     from matplotlib.backends.backend_qt5agg import (
             FigureCanvasQTAgg as FigureCanvas,
             NavigationToolbar2QT as NavigationToolbar)
-    matplotlib.use('Qt5Agg')
+
 except ImportError:    
     import PyQt4.QtCore as QtCore
     import PyQt4.QtGui as QtGui
@@ -25,18 +21,24 @@ except ImportError:
     from matplotlib.backends.backend_qt4agg import (
             FigureCanvasQTAgg as FigureCanvas,
             NavigationToolbar2QT as NavigationToolbar)
-    matplotlib.use('Qt4Agg')
+
+import pyqtgraph as pg
+
+import ICRH_Conditioning as condi
+import ICRH_FileIO as io
+
+# switch default plotting scheme to white
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
 
 class AppForm(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)        
         self.create_main_frame()
         self.setGeometry(0, 0, 1600, 600)
-        self.setWindowTitle("WEST ICRH Conditoning Analysis")
-
+        self.setWindowTitle("WEST ICRH Conditoning Data Analysis")
         # sync remote files to local
         self.sync_files()
-
         # fill the shot table with the date of the last shots
         self.update_shot_table()
         # default plotted data are from last shot file
@@ -46,14 +48,17 @@ class AppForm(QMainWindow):
 
     def create_main_frame(self):
         self.main_frame = QWidget()
-        # Figure and toolbar
-        self.fig = Figure((10.0, 6.0), dpi=100)
-        self.canvas = FigureCanvas(self.fig)
-        self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
-        self.canvas.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.canvas.setFocus()
-        # Embedding in Qt4
-        self.canvas.setParent(self.main_frame)
+        # pyqtgraph Figures
+        self.l = pg.GraphicsLayoutWidget(border=(100, 100, 100))
+        self.PG = self.l.addPlot(row=0, col=0, name='Pi,Pr Gauche')
+        self.PD = self.l.addPlot(row=0, col=1, name='Pi,Pr Droite')
+        self.PhG = self.l.addPlot(row=1, col=0, name='Phase V1-V3 Gauche')
+        self.PhD = self.l.addPlot(row=1, col=1, name='Phase V2-V4 Droite')
+        self.VG = self.l.addPlot(row=2, col=0, name='Tensions V1,V3 Gauche')
+        self.VD = self.l.addPlot(row=2, col=1, name='Tensions V2,V4 Droite')
+        self.pTransG = self.l.addPlot(row=3, col=0, name='Pression Gauche')
+        self.pTransD = self.l.addPlot(row=3, col=1, name='Pression Droit')
+        
         # Default Fonts
         button_default_font = QtGui.QFont('SansSerif', 16)
         item_default_font = QtGui.QFont('SansSerif', 12)
@@ -85,13 +90,12 @@ class AppForm(QMainWindow):
         vbox_metadata.addWidget(self.metadata_table)
 
         vbox_canvas = QVBoxLayout()
-        vbox_canvas.addWidget(self.canvas)  # the matplotlib canvas
-        vbox_canvas.addWidget(self.mpl_toolbar)
+        vbox_canvas.addWidget(self.l)
 
         hbox = QHBoxLayout()
         hbox.addLayout(vbox_shots)
         hbox.addLayout(vbox_metadata)
-        hbox.addLayout(vbox_canvas)
+        hbox.addLayout(vbox_canvas, 2) # canvas is twice bigger than all others
 
         self.main_frame.setLayout(hbox)
         self.setCentralWidget(self.main_frame)
@@ -151,22 +155,28 @@ class AppForm(QMainWindow):
         if data.empty:
             print('Empty data!')
         else:
-            self.fig.clear()
-            self.axes1 = self.fig.add_subplot(221)
-            self.axes2 = self.fig.add_subplot(222)
-            self.axes3 = self.fig.add_subplot(223)
-            self.axes4 = self.fig.add_subplot(224)
-            time = data.index/1e3  # display time in ms
-            self.axes1.plot(time, data.PiG/10, time, data.PrG/10, alpha=0.7)
-            self.axes2.plot(time, data.PiD/10, time, data.PrD/10, alpha=0.7)
-            self.axes3.plot(time, data.V1, time, data.V2, alpha=0.7)
-            self.axes4.plot(time, data.V3, time, data.V4, alpha=0.7)
-            self.axes3.set_xlabel('Time [ms]')
-            self.axes4.set_xlabel('Time [ms]')
-            self.axes1.set_ylabel('Power [kW]')
-            self.axes3.set_ylabel('Voltage [V]')
-            self.fig.tight_layout()
-            self.canvas.draw()
+            # NB: pandas -> np arrays for pyqtgraph compatibility
+            time = data.index.values/1e3  # display time in ms
+            # Pi,Pr Gauche
+            self.PG.plot(pen='b', x=time, y=data.PiG.values/10, clear=True)
+            self.PG.plot(pen='r', x=time, y=data.PrG.values/10, clear=True)
+            # Pi,Pr Droite
+            self.PD.plot(pen='b', x=time, y=data.PiD.values/10, clear=True)
+            self.PD.plot(pen='r', x=time, y=data.PrD.values/10, clear=True)
+            # Phase Gauche
+            self.PhG.plot(pen='b', x=time, y=data['Ph(V1-V3)'].values, clear=True)
+            self.PhG.autoRange()
+            # Phase Droite
+            self.PhD.plot(pen='b', x=time, y=data['Ph(V2-V4)'].values, clear=True)
+            # Tensions Gauche
+            self.VG.plot(pen='b', x=time, y=data.V1.values, clear=True)
+            self.VG.plot(pen='r', x=time, y=data.V3.values, clear=True)
+            # Tensions Droite
+            self.VD.plot(pen='b', x=time, y=data.V2.values, clear=True)
+            self.VD.plot(pen='r', x=time, y=data.V4.values, clear=True)
+            # Pression dans le transfo gauche et droit
+            self.pTransG.plot(pen='b', x=time, y=data.Vide_gauche.values, clear=True)
+            self.pTransD.plot(pen='b', x=time, y=data.Vide_droit.values, clear=True)
 
 
 def main():
--- gui_fastacq.py
+++ gui_fastacq.py
@@ -1,8 +1,4 @@
 import sys
-from matplotlib.figure import Figure
-import ICRH_FastData as fast
-import ICRH_FileIO as io
-import matplotlib
 
 # Qt5/Qt4 compatibility
 try: 
@@ -14,7 +10,6 @@ try:
     from matplotlib.backends.backend_qt5agg import (
             FigureCanvasQTAgg as FigureCanvas,
             NavigationToolbar2QT as NavigationToolbar)
-    matplotlib.use('Qt5Agg')
 except ImportError:    
     import PyQt4.QtCore as QtCore
     import PyQt4.QtGui as QtGui
@@ -24,13 +19,15 @@ except ImportError:
     from matplotlib.backends.backend_qt4agg import (
             FigureCanvasQTAgg as FigureCanvas,
             NavigationToolbar2QT as NavigationToolbar)
-    matplotlib.use('Qt4Agg')
 
 import pyqtgraph as pg
+
+import ICRH_FastData as fast
+import ICRH_FileIO as io
+
 # switch default plotting scheme to white
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
-
 
 class AppForm(QMainWindow):
     def __init__(self, parent=None):
@@ -43,22 +40,12 @@ class AppForm(QMainWindow):
         # sync remote files to local
         self.sync_files() 
         # fill the shot list with the list of available shots
-        self.update_shot_list()
-        
+        self.update_shot_list()        
         # default plotted data are from last shot file
-
         self.update_plot()
 
     def create_main_frame(self):
         self.main_frame = QWidget()
-        # Figure and toolbar
-        self.fig = Figure((20.0, 6.0), dpi=100)
-        self.canvas = FigureCanvas(self.fig)
-        self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
-        self.canvas.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.canvas.setFocus()
-        # Embedding in Qt4
-        self.canvas.setParent(self.main_frame)
         # Default Fonts
         button_default_font = QtGui.QFont('SansSerif', 16)
         item_default_font = QtGui.QFont('SansSerif', 12)
@@ -83,9 +70,6 @@ class AppForm(QMainWindow):
         vbox_shots.addWidget(self.shot_list_widget)
         vbox_shots.addWidget(self.plot_button)
         
-        vbox_canvas = QVBoxLayout()
-        #vbox_canvas.addWidget(self.canvas)  # the matplotlib canvas
-        #vbox_canvas.addWidget(self.mpl_toolbar)
         self.l = pg.GraphicsLayoutWidget(border=(100,100,100))
         self.PowQ1 = self.l.addPlot(row=0, col=0, name='Pow')
         self.PhaQ1 = self.l.addPlot(row=0, col=1, name='Pha')
@@ -99,26 +83,23 @@ class AppForm(QMainWindow):
         self.PowQ4.setXLink(self.PowQ1)
         self.PhaQ4 = self.l.addPlot(row=2, col=1, name='Pha')
         self.PhaQ4.setXLink(self.PhaQ1)
-                
+
+        vbox_canvas = QVBoxLayout()                
         vbox_canvas.addWidget(self.l)
-
-
 
         hbox = QHBoxLayout()
         hbox.addLayout(vbox_shots, 1)
         hbox.addLayout(vbox_canvas, 6)
 
-
         self.main_frame.setLayout(hbox)
         self.setCentralWidget(self.main_frame)
-        
-    
+
     def get_local_file_list(self):
         return io.list_local_files(local_data_path = 'data/Fast_Data/')
-    
+
     def get_remote_file_list(self):
         return io.list_remote_files(remote_path='/media/ssd/Fast_Data')
-    
+
     def sync_files(self):
         '''Synchronize remote files to local directory'''
         self.remote_files = self.get_remote_file_list()
@@ -126,7 +107,7 @@ class AppForm(QMainWindow):
                                       local_data_path = 'data/Fast_Data/',
                                       remote_data_path='/media/ssd/Fast_Data')
         self.local_files = self.get_local_file_list()
-            
+
     def update_shot_list(self):
         '''Update the list widget '''
         self.shot_list = fast.get_shot_list(self.local_files)
@@ -163,16 +144,8 @@ class AppForm(QMainWindow):
         if not self.data.get(shot):
             print('Converting data of shot #{}'.format(shot))
             self.data[shot] = fast.FastData(shot)
-        
-    
+
     def update_plot(self):
-        self.fig.clear()
-        self.axes_PowQ1 = self.fig.add_subplot(231) # amplitudes
-        self.axes_PowQ2 = self.fig.add_subplot(232)
-        self.axes_PowQ4 = self.fig.add_subplot(233)
-        self.axes_PhaQ1 = self.fig.add_subplot(234) # amplitudes
-        self.axes_PhaQ2 = self.fig.add_subplot(235)
-        self.axes_PhaQ4 = self.fig.add_subplot(236)
         try:
             if  not self.data[self.shot].Q1_amplitude.empty:
                 self.PowQ1.plot(pen='b', x=self.data[self.shot].Q1_amplitude['PiG'].index/1e6, 
@@ -193,28 +166,12 @@ class AppForm(QMainWindow):
         except AttributeError as e:
             print('No shot exist yet!')
             print(e)
-                                         
-#        self.axes3 = self.fig.add_subplot(223)
-#        self.axes4 = self.fig.add_subplot(224)
-#        
-#        time = data.index/1e3 # display time in ms
-#        self.axes1.plot(time, data.PiG/10, time, data.PrG/10, alpha=0.7)
-#        self.axes2.plot(time, data.PiD/10, time, data.PrD/10, alpha=0.7)
-#        self.axes3.plot(time, data.V1, time, data.V2, alpha=0.7)
-#        self.axes4.plot(time, data.V3, time, data.V4, alpha=0.7)
-#        self.axes3.set_xlabel('Time [ms]')
-#        self.axes4.set_xlabel('Time [ms]')
-#        self.axes1.set_ylabel('Power [kW]')
-#        self.axes3.set_ylabel('Voltage [V]')
-#        self.fig.tight_layout()
-        self.canvas.draw()
-
 
 
 def main():
     # Hack to be able to run the code from spyder
     # taken from http://stackoverflow.com/questions/19459299/running-a-pyqt-application-twice-from-one-prompt-in-spyder
-    if QtCore.QCoreApplication.instance() != None:
+    if QtCore.QCoreApplication.instance() is not None:
         app = QtCore.QCoreApplication.instance()
     else:
         app = QApplication(sys.argv)
